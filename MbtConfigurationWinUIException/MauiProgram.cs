1682182ca8129487935cc7a4c14a25517c3c3fd7
Ronan Burke <ronan.burke@civica.co.uk>
2023-01-05 15:09:01 +0000

    Add MBT configs

--- MbtConfigurationWinUIException/MauiProgram.cs
+++ MbtConfigurationWinUIException/MauiProgram.cs
@@ -1,18 +1,28 @@
-﻿namespace MbtConfigurationWinUIException;
+﻿using Mobile.BuildTools.Configuration;
+
+namespace MbtConfigurationWinUIException;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .RegisterAppServices()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
+
+    public static MauiAppBuilder RegisterAppServices(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddSingleton(ConfigurationManager.Current);
+
+        return mauiAppBuilder;
+    }
 }
--- MbtConfigurationWinUIException/Platforms/Windows/App.xaml.cs
+++ MbtConfigurationWinUIException/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Mobile.BuildTools.Configuration;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,15 +10,33 @@ namespace MbtConfigurationWinUIException.WinUI;
 /// </summary>
 public partial class App : MauiWinUIApplication
 {
-	/// <summary>
-	/// Initializes the singleton application object.  This is the first line of authored code
-	/// executed, and as such is the logical equivalent of main() or WinMain().
-	/// </summary>
-	public App()
-	{
-		this.InitializeComponent();
-	}
+    /// <summary>
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public App()
+    {
+        this.InitializeComponent();
 
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+        ConfigurationManager.Init(true);
+        TransformConfiguration(ConfigurationManager.Current);
+    }
+
+    public static void TransformConfiguration(IConfigurationManager configurationManager)
+    {
+        // Transform config based on current environment defined by config
+        configurationManager.Reset();
+
+        var targetEnvironment = configurationManager.AppSettings["CurrentEnvironment"];
+
+        if (!configurationManager.EnvironmentExists(targetEnvironment))
+        {
+            Console.WriteLine($"Unable to target configured environment: {targetEnvironment} because it does not exist");
+            return;
+        }
+
+        configurationManager.Transform(targetEnvironment);
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
-
